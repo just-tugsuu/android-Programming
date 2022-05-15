fd8fd72deefb4d8b673a05f1c69b1db556ad1e66
signal451 <uwugoth266@gmail.com>
2022-05-15 14:46:32 +0800

    chore: add new layouts and few intents between activity

--- app/src/main/java/com/example/androidplatform/LoginActiviy.java
+++ app/src/main/java/com/example/androidplatform/LoginActiviy.java
@@ -1,5 +1,7 @@
 package com.example.androidplatform;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -7,11 +9,14 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.button.MaterialButton;
+
 public class LoginActiviy extends AppCompatActivity {
 
     private TextView toolbarTitle;
     private ImageView backbtn;
-
+    private MaterialButton loginBtn;
+    private Context context = LoginActiviy.this;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,10 +25,16 @@ public class LoginActiviy extends AppCompatActivity {
 
         toolbarTitle = findViewById(R.id.toolbarTitle);
         backbtn = findViewById(R.id.back_btn);
+        loginBtn = findViewById(R.id.LoginBtn);
         toolbarTitle.setText("Нэвтрэх");
 
         backbtn.setOnClickListener(view -> {
             finish();
+        });
+
+        loginBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(context, LanguageSelectionActivity.class);
+            startActivity(intent);
         });
 
     }
